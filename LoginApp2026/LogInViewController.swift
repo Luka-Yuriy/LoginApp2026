b1521b78578cb1327ced33e5f17d4daa6964e5d3
Yuri Luka <lukayurii.work@gmail.com>
2025-07-09 18:55:27 +0500

    Alert cintroller

--- LoginApp2026/LogInViewController.swift
+++ LoginApp2026/LogInViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    private let userName = "User"
+    private let password = "1234"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonDidTapped() {
+    }
+    
+    @IBAction func userNameHintButtomDidTapped() {
+        showAlert(title: "Oops!", message: "Your name is User")
+    }
+    
+    @IBAction func userPasswordHintButtonDidTapped() {
+        showAlert(title: "Oops!", message: "Your password is 1234")
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordTF.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+        
+    }
 }
 
